6fffeb2216e9e354b8bfe040873ad4e8b6548f60
tsukumaru <came.and.rabbit@gmail.com>
2014-09-17 13:37:29 +0900

    顔の中心の色をとってこようとする試み

--- Teno_Repi/Teno_Repi.pde
+++ Teno_Repi/Teno_Repi.pde
@@ -1,11 +1,16 @@
 import processing.video.*;
-// import java.awt.*;
+import gab.opencv.*;
+import java.awt.*;
 
 Capture video;
+OpenCV opencv;
 
 void setup() {
   size(640, 480);
   video = new Capture(this, width, height);
+
+  opencv = new OpenCV(this, width, height);
+  opencv.loadCascade(OpenCV.CASCADE_FRONTALFACE);
 
   video.start();
   loadPixels(); //windowのピクセルにpixels[]で参照できる
@@ -17,28 +22,38 @@ void draw() {
     video.read();
     video.loadPixels(); //captureしたカメラ画像のピクセルを取得
 
-    colorMode(HSB);
+    opencv.loadImage(video);
+   image(video, 0, 0);
+
+    //colorMode(HSB);
     // image(video, 0, 0);
-    filter(BLUR, 6); //パラメータ無しでガウシアンフィルタ
-    for (int y = 0; y < video.height; ++y) {
-      for (int x = 0; x < video.width; ++x) {
-        toFleshColorBinarizationIm(video, x, y);
-       
-      }
-    }
-    updatePixels();
+    //filter(BLUR); //パラメータ無しでガウシアンフィルタ
+     for (int y = 0; y < video.height; ++y) {
+       for (int x = 0; x < video.width; ++x) {
+         toFleshColorBinarizationIm(video, x, y);
+        
+       }
+     }
+     updatePixels();
+  // image(video, 0, 0);
+    
   }
 }
 
 //肌色で2値化
 void toFleshColorBinarizationIm(Capture video, int x, int y){
  int pixelColor = video.pixels[y*video.width + x];
+ Rectangle[] faces = opencv.detect();
+ int face_center = video.pixels[(faces[0].y + faces[0].height / 2) 
+ * video.width + (faces[0].x + faces[0].width / 2)];
 
- float h = hue(pixelColor);
- // float s = saturation(pixelColor);
- // float v = brightness(pixelColor);
- if (h >= 0.0 && h <= 30.0)
-   pixels[y*video.width + x] = color(0);
-  else
-   pixels[y*video.width + x] = color(180);
+ float r = red(pixelColor);
+ float g = green(pixelColor);
+ float b = blue(pixelColor);
+ if (red(face_center) - 10 <= r && r <= red(face_center) + 10 
+  && green(face_center) - 10 <= g && g <= green(face_center) + 10
+  && blue(face_center) - 10 <= b && b <= blue(face_center) + 10)
+   pixels[y*video.width + x] = 0;
+ else
+   pixels[y*video.width + x] = 180;
 }
--- Teno_Repi/build-tmp/source/Teno_Repi.java
+++ Teno_Repi/build-tmp/source/Teno_Repi.java
@@ -4,6 +4,8 @@ import processing.event.*;
 import processing.opengl.*; 
 
 import processing.video.*; 
+import gab.opencv.*; 
+import java.awt.*; 
 
 import java.util.HashMap; 
 import java.util.ArrayList; 
@@ -17,13 +19,18 @@ import java.io.IOException;
 public class Teno_Repi extends PApplet {
 
 
-// import java.awt.*;
+
+
 
 Capture video;
+OpenCV opencv;
 
 public void setup() {
   size(640, 480);
   video = new Capture(this, width, height);
+
+  opencv = new OpenCV(this, width, height);
+  opencv.loadCascade(OpenCV.CASCADE_FRONTALFACE);
 
   video.start();
   loadPixels(); //window\u306e\u30d4\u30af\u30bb\u30eb\u306bpixels[]\u3067\u53c2\u7167\u3067\u304d\u308b
@@ -35,30 +42,40 @@ public void draw() {
     video.read();
     video.loadPixels(); //capture\u3057\u305f\u30ab\u30e1\u30e9\u753b\u50cf\u306e\u30d4\u30af\u30bb\u30eb\u3092\u53d6\u5f97
 
-    colorMode(HSB);
+    opencv.loadImage(video);
+   image(video, 0, 0);
+
+    //colorMode(HSB);
     // image(video, 0, 0);
-    filter(BLUR, 6); //\u30d1\u30e9\u30e1\u30fc\u30bf\u7121\u3057\u3067\u30ac\u30a6\u30b7\u30a2\u30f3\u30d5\u30a3\u30eb\u30bf
-    for (int y = 0; y < video.height; ++y) {
-      for (int x = 0; x < video.width; ++x) {
-        toFleshColorBinarizationIm(video, x, y);
-       
-      }
-    }
-    updatePixels();
+    //filter(BLUR); //\u30d1\u30e9\u30e1\u30fc\u30bf\u7121\u3057\u3067\u30ac\u30a6\u30b7\u30a2\u30f3\u30d5\u30a3\u30eb\u30bf
+     for (int y = 0; y < video.height; ++y) {
+       for (int x = 0; x < video.width; ++x) {
+         toFleshColorBinarizationIm(video, x, y);
+        
+       }
+     }
+     updatePixels();
+  // image(video, 0, 0);
+    
   }
 }
 
 //\u808c\u8272\u30672\u5024\u5316
 public void toFleshColorBinarizationIm(Capture video, int x, int y){
  int pixelColor = video.pixels[y*video.width + x];
+ Rectangle[] faces = opencv.detect();
+ int face_center = video.pixels[(faces[0].y + faces[0].height / 2) 
+ * video.width + (faces[0].x + faces[0].width / 2)];
 
- float h = hue(pixelColor);
- // float s = saturation(pixelColor);
- // float v = brightness(pixelColor);
- if (h >= 0.0f && h <= 30.0f)
-   pixels[y*video.width + x] = color(0);
-  else
-   pixels[y*video.width + x] = color(180);
+ float r = red(pixelColor);
+ float g = green(pixelColor);
+ float b = blue(pixelColor);
+ if (red(face_center) - 10 <= r && r <= red(face_center) + 10 
+  && green(face_center) - 10 <= g && g <= green(face_center) + 10
+  && blue(face_center) - 10 <= b && b <= blue(face_center) + 10)
+   pixels[y*video.width + x] = 0;
+ else
+   pixels[y*video.width + x] = 100;
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Teno_Repi" };
